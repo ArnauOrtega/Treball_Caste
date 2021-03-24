2436d2ee46a339b81ca3d8a829d8c78b837135bf
Arnau Ortega <arnauortegathau@gmail.com>
2021-03-24 13:34:55 +0100

    soc dawn

--- pistes.py
+++ pistes.py
@@ -1,5 +1,6 @@
 from playsound import playsound
 import serial
+import os
 pista = ["pista1", "pista2", "pista3","pista4"]
 ser = serial.Serial('/dev/ttyACM0',baudrate = 9600, timeout=1)
 while 1:
@@ -8,4 +9,5 @@ while 1:
     except:continue
     for x in range(4):
         if num == x:
-            playsound("/home/ubuntu/pistes/"+pista[x]+".mp3")
+            sound =  os.getcwd()+"/pistes/"+pista[x]+".mp3"
+            playsound(sound)
